c4c5cd01e0e0ef03648ee393d7885da0c1166395
amanpatel59 <ap707359@gmail.com>
2022-09-19 23:26:49 +0530

    not_complete

---
+++ searching/allocate_minimum_no._of_pages.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// PATA NAHI KYA BAWAAL HAI
+// NEED TO LOOK WITH PROPER FOCUS 
+
+int main()
+{
+    int n ;
+    cout<<"enter the sizze of the array = ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"enter the elements of array "<<endl;
+
+    for(int i = 0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+}
